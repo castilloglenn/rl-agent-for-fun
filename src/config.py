ad1219f5c684e67c7976975bf7bb0afeb6bab83a
castilloglenn <castilloglenn@ymail.com>
2023-11-26 20:44:02 +0800

    style: remove comments

--- src/config.py
+++ src/config.py
@@ -4,16 +4,16 @@ from ml_collections import ConfigDict
 def get_config() -> ConfigDict:
     config = ConfigDict()
 
-    config.num_iterations = 20000  # @param {type:"integer"}
+    config.num_iterations = 20000
 
-    config.initial_collect_steps = 100  # @param {type:"integer"}
-    config.collect_steps_per_iteration = 1  # @param {type:"integer"}
+    config.initial_collect_steps = 100
+    config.collect_steps_per_iteration = 1
 
-    config.batch_size = 64  # @param {type:"integer"}
-    config.learning_rate = 1e-3  # @param {type:"number"}
-    config.log_interval = 200  # @param {type:"integer"}
+    config.batch_size = 64
+    config.learning_rate = 1e-3
+    config.log_interval = 200
 
-    config.num_eval_episodes = 10  # @param {type:"integer"}
-    config.eval_interval = 1000  # @param {type:"integer"}
+    config.num_eval_episodes = 10
+    config.eval_interval = 1000
 
     return config
