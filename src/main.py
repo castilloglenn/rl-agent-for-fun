e195aaec8e3b6de95b52cfe598c212ffe8b48597
castilloglenn <castilloglenn@ymail.com>
2023-11-26 19:28:01 +0800

    feat: docs sample env test run

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
+import numpy as np
 from absl import flags
 from tf_agents.environments import utils
 
 from src.environment import CardGameEnv
+from src.helpers import print_spaced
 
 FLAGS = flags.FLAGS
 
@@ -11,4 +13,22 @@ class Main:
         environment = CardGameEnv()
         utils.validate_py_environment(environment=environment)
 
-        print("Done.")
+        get_new_card_action = np.array(0, dtype=np.int32)
+        end_round_action = np.array(1, dtype=np.int32)
+
+        environment = CardGameEnv()
+        time_step = environment.reset()
+        print_spaced(time_step)
+        cumulative_reward = time_step.reward
+
+        for _ in range(3):
+            time_step = environment.step(get_new_card_action)
+            print_spaced(time_step)
+            cumulative_reward += time_step.reward
+
+        time_step = environment.step(end_round_action)
+        print_spaced(time_step)
+        cumulative_reward += time_step.reward
+        print_spaced("Final Reward = ", cumulative_reward)
+
+        print_spaced("Done.")
