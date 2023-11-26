2d035dd6a877323ced49195389c715a9e29a3ef9
castilloglenn <castilloglenn@ymail.com>
2023-11-26 20:31:04 +0800

    refactor: remove unnecessary arg

--- src/helpers.py
+++ src/helpers.py
@@ -1,3 +1,3 @@
-def print_spaced(msg: str, *args) -> None:
+def print_spaced(*values) -> None:
     double_spaced = "\n\n"
-    print(f"{msg}{' '.join(map(str, args))}", end=double_spaced)
+    print(f"{' '.join(map(str, values))}", end=double_spaced)
