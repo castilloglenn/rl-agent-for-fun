73b3e6591f0163dee18282f2086490fef2a5858e
castilloglenn <castilloglenn@ymail.com>
2023-11-27 19:24:39 +0800

    feat: create batch

--- src/helpers.py
+++ src/helpers.py
@@ -1,4 +1,6 @@
 import keras
+import numpy as np
+import tensorflow as tf
 
 
 def print_spaced(*values) -> None:
@@ -30,3 +32,31 @@ def compute_avg_return(environment, policy, num_episodes=10):
 
     avg_return = total_return / num_episodes
     return avg_return.numpy()[0]
+
+
+def create_batch(sampled_experience):
+    states = []
+    actions = []
+    next_states = []
+    rewards = []
+
+    for step in sampled_experience:
+        states.append(step.observation)
+        actions.append(step.action)
+        next_states.append(step.next_step.observation)
+        rewards.append(step.reward)
+
+    states_batch = tf.convert_to_tensor(np.array(states), dtype=tf.float32)
+    actions_batch = tf.convert_to_tensor(np.array(actions), dtype=tf.int32)
+    next_states_batch = tf.convert_to_tensor(
+        np.array(next_states),
+        dtype=tf.float32,
+    )
+    rewards_batch = tf.convert_to_tensor(np.array(rewards), dtype=tf.float32)
+
+    return {
+        "states": states_batch,
+        "actions": actions_batch,
+        "next_states": next_states_batch,
+        "rewards": rewards_batch,
+    }
