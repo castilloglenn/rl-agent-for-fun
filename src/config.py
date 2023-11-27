267f4305e2e3f3295d34ce7b7394791c6fcdf1b5
castilloglenn <castilloglenn@ymail.com>
2023-11-27 18:02:35 +0800

    feat: typing

--- src/config.py
+++ src/config.py
@@ -4,19 +4,19 @@ from ml_collections import ConfigDict
 def get_agent_config() -> ConfigDict:
     config = ConfigDict()
 
-    config.fully_connected_layers = (100, 50)
+    config.fully_connected_layers: tuple[*int] = (100, 50)
 
-    config.num_iterations = 20000
+    config.num_iterations: int = 20_000
 
-    config.initial_collect_steps = 100
-    config.collect_steps_per_iteration = 1
+    config.initial_collect_steps: int = 100
+    config.collect_steps_per_iteration: int = 1
 
-    config.batch_size = 64
-    config.learning_rate = 1e-3
-    config.log_interval = 200
+    config.batch_size: int = 64
+    config.learning_rate: float = 1e-3
+    config.log_interval: int = 200
 
-    config.num_eval_episodes = 10
-    config.eval_interval = 1000
+    config.num_eval_episodes: int = 10
+    config.eval_interval: int = 1_000
 
     return config
 
@@ -24,8 +24,8 @@ def get_agent_config() -> ConfigDict:
 def get_fast_traffic_config() -> ConfigDict:
     config = ConfigDict()
 
-    config.lane_length = 5
-    config.spawn_rate = 0.4
-    config.total_ticks = 50
+    config.lane_length: int = 5
+    config.spawn_rate: float = 0.4
+    config.total_ticks: int = 50
 
     return config
