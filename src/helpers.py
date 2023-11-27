73979cd24a3910c2216e1c466266ac10076acc00
castilloglenn <castilloglenn@ymail.com>
2023-11-27 12:30:04 +0800

    feat: dense layer helper

--- src/helpers.py
+++ src/helpers.py
@@ -1,3 +1,16 @@
+import keras
+
+
 def print_spaced(*values) -> None:
     double_spaced = "\n\n"
     print(f"{' '.join(map(str, values))}", end=double_spaced)
+
+
+def dense_layer(num_units):
+    return keras.layers.Dense(
+        num_units,
+        activation=keras.activations.relu,
+        kernel_initializer=keras.initializers.VarianceScaling(
+            scale=2.0, mode="fan_in", distribution="truncated_normal"
+        ),
+    )
