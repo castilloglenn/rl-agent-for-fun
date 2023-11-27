e0caf7cff1a007a16183c7f2dcba6a408e919530
castilloglenn <castilloglenn@ymail.com>
2023-11-27 20:15:09 +0800

    halt: requires reverb lib to proceed

--- src/helpers.py
+++ src/helpers.py
@@ -1,11 +1,4 @@
 import keras
-import numpy as np
-import tensorflow as tf
-
-
-def print_spaced(*values) -> None:
-    double_spaced = "\n\n"
-    print(f"{' '.join(map(str, values))}", end=double_spaced)
 
 
 def dense_layer(num_units):
@@ -32,31 +25,3 @@ def compute_avg_return(environment, policy, num_episodes=10):
 
     avg_return = total_return / num_episodes
     return avg_return.numpy()[0]
-
-
-def create_batch(sampled_experience):
-    states = []
-    actions = []
-    next_states = []
-    rewards = []
-
-    for step in sampled_experience:
-        states.append(step.observation)
-        actions.append(step.action)
-        next_states.append(step.next_step.observation)
-        rewards.append(step.reward)
-
-    states_batch = tf.convert_to_tensor(np.array(states), dtype=tf.float32)
-    actions_batch = tf.convert_to_tensor(np.array(actions), dtype=tf.int32)
-    next_states_batch = tf.convert_to_tensor(
-        np.array(next_states),
-        dtype=tf.float32,
-    )
-    rewards_batch = tf.convert_to_tensor(np.array(rewards), dtype=tf.float32)
-
-    return {
-        "states": states_batch,
-        "actions": actions_batch,
-        "next_states": next_states_batch,
-        "rewards": rewards_batch,
-    }
--- src/main.py
+++ src/main.py
@@ -4,24 +4,32 @@ import keras
 import tensorflow as tf
 from absl import flags
 from tf_agents.agents.dqn import dqn_agent
+
+# from tf_agents.drivers.py_driver import PyDriver
 from tf_agents.environments import tf_py_environment
 from tf_agents.networks import sequential
+
+# from tf_agents.policies.py_tf_eager_policy import PyTFEagerPolicy
+from tf_agents.specs import tensor_spec
 from tf_agents.utils import common
 
 from src.environments.fast_traffic import FastTrafficEnv
-from src.helpers import dense_layer, print_spaced
+from src.helpers import compute_avg_return, dense_layer
 
 FLAGS = flags.FLAGS
-print = print_spaced  # Temporary
 
 
 class Main:
     def __init__(self):
-        train_env = tf_py_environment.TFPyEnvironment(FastTrafficEnv())
-        # test_env = tf_py_environment.TFPyEnvironment(FastTrafficEnv())
+        env = FastTrafficEnv()
+        train_py_env = FastTrafficEnv()
+        test_py_env = FastTrafficEnv()
 
-        time_step_spec = train_env.time_step_spec()
-        action_spec = train_env.action_spec()
+        train_tf_env = tf_py_environment.TFPyEnvironment(train_py_env)
+        test_tf_env = tf_py_environment.TFPyEnvironment(test_py_env)
+
+        time_step_spec = train_tf_env.time_step_spec()
+        action_spec = tensor_spec.from_spec(env.action_spec())
         num_actions = action_spec.maximum - action_spec.minimum + 1
 
         fc_layers = FLAGS.agent.fully_connected_layers
@@ -51,5 +59,61 @@ class Main:
             train_step_counter=train_step_counter,
         )
         agent.initialize()
+
+        agent.train = common.function(agent.train)
+        agent.train_step_counter.assign(0)
+
+        num_eval_episodes = FLAGS.agent.num_eval_episodes
+        avg_return = compute_avg_return(
+            test_tf_env,
+            agent.policy,
+            num_eval_episodes,
+        )
+        # returns = [avg_return]
+        print(f"Average return before training: {avg_return:,.2f}")
+
+        # time_step = train_py_env.reset()
+
+        # collect_steps_per_iteration = FLAGS.agent.collect_steps_per_iteration
+        # collect_driver = PyDriver(
+        #     env=env,
+        #     policy=PyTFEagerPolicy(
+        #         agent.collect_policy,
+        #         use_tf_function=True,
+        #     ),
+        #     observers=[],
+        #     max_steps=collect_steps_per_iteration,
+        # )
+
+        # experience_buffer = []
+
+        # batch_size = FLAGS.agent.batch_size
+        # max_buffer_size = FLAGS.agent.max_buffer_size
+        # num_iterations = FLAGS.agent.num_iterations
+        # log_interval = FLAGS.agent.log_interval
+        # eval_interval = FLAGS.agent.eval_interval
+
+        # for _ in range(num_iterations):
+        #     time_step, _ = collect_driver.run(time_step)
+        #     experience_buffer.append(time_step)
+
+        #     if len(experience_buffer) > max_buffer_size:
+        #         experience_buffer.pop(0)
+
+        #     batch = min(batch_size, len(experience_buffer))
+        #     sampled_experience = random.sample(experience_buffer, batch)
+        #     tf_batch = create_batch(sampled_experience)
+        #     train_loss = agent.train(tf_batch).loss
+
+        #     step = agent.train_step_counter.numpy()
+        #     if step % log_interval == 0:
+        #         print(f"step = {step}: loss = {train_loss}")
+
+        #     if step % eval_interval == 0:
+        #         avg_return = compute_avg_return(
+        #             test_tf_env, agent.policy, num_eval_episodes
+        #         )
+        #         print(f"step = {step}: Average Return = {avg_return}")
+        #         returns.append(avg_return)
 
         print("Done.")
