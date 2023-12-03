21443f8a749a80e9d2f174c5226195c9ef882615
castilloglenn <castilloglenn@ymail.com>
2023-12-03 22:48:32 +0800

    feat: basic pygame setup

--- src/envs/maze_car.py
+++ src/envs/maze_car.py
@@ -1,10 +1,27 @@
+import pygame
+from absl import flags
+
 from src.envs.base import Environment, GameOver, Reward, Score
+
+FLAGS = flags.FLAGS
+
+WIDTH = FLAGS.maze_car.window.width
+HEIGHT = FLAGS.maze_car.window.height
+FPS = FLAGS.maze_car.display.fps
 
 
 class MazeCarEnv(Environment):
     def __init__(self, user_inputs: bool = False) -> None:
         self.user_inputs = user_inputs
-        print("MazeCarEnv")
+
+        # pylint: disable=E1101
+        pygame.init()
+        pygame.display.set_caption(FLAGS.maze_car.window.title)
+
+        self.clock = pygame.time.Clock()
+        self.display = pygame.display.set_mode((WIDTH, HEIGHT))
+
+        self.reset()
 
     def reset(self) -> None:
         pass
