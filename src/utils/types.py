c3181b088ed46512d903c4be8c24c1a3d163f4c4
castilloglenn <castilloglenn@ymail.com>
2024-01-02 17:30:14 +0800

    feat: colors

--- src/utils/types.py
+++ src/utils/types.py
@@ -15,3 +15,14 @@ class WindowConstants:
     height: int
     half_height: int
     quarter_height: int
+
+
+ColorValue = tuple[int, int, int]
+
+
+class Colors:
+    WHITE: ColorValue = (255, 255, 255)
+    BLACK: ColorValue = (0, 0, 0)
+    RED: ColorValue = (255, 0, 0)
+    GREEN: ColorValue = (0, 255, 0)
+    BLUE: ColorValue = (0, 0, 255)
