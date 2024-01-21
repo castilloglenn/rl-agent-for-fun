b045f7f586933159f3f758148130629e074f5d77
castilloglenn <castilloglenn@ymail.com>
2024-01-21 21:52:45 +0800

    feat: tick elapsed

--- src/envs/maze_car/models/display_state.py
+++ src/envs/maze_car/models/display_state.py
@@ -1,10 +1,22 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+
+from absl import flags
 from pygame import Surface
 
 from pygame.time import Clock
+
+FLAGS = flags.FLAGS
 
 
 @dataclass
 class DisplayState:
     clock: Clock = Clock()
     display: Surface = Surface((0, 0))
+
+    tick_elapsed: float = field(init=False)
+
+    def __post_init__(self):
+        self.tick_elapsed = 0.0
+
+    def tick(self):
+        self.tick_elapsed = self.clock.tick(FLAGS.maze_car.display.fps) / 1000.0
